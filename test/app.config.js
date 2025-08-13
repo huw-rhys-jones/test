636f5dc374479709a3bc0a74bcf7f086b03d343f
Huw Jones <huwjones37@gmail.com>
2025-08-13 23:34:34 +0100

    start

---
+++ test/app.config.js
@@ -0,0 +1,48 @@
+import "dotenv/config";
+
+export default ({ config }) => ({
+  ...config,
+  name: "Test",
+  slug: "test",
+  scheme: "com.caistec.test", // 👈 ADD THIS
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/images/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.caistec.test",
+    buildNumber: "15",
+  },
+  android: {
+    package: "com.caistec.test",
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/images/favicon.png",
+  },
+  extra: {
+    eas: {
+    //   projectId: "5b149386-fb46-4d4d-8308-fde7bcff2f37",
+    },
+    // FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    // FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    // FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    // FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    // FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    // FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    // GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+    // GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID,
+    // GOOGLE_IOS_CLIENT_ID: process.env.GOOGLE_IOS_CLIENT_ID
+  },
+});
